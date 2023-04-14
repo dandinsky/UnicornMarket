b54f735c954a989bedb7ffa4251960f3f6a6c7ae
dandinsky <dora.marmorosa@gmail.com>
2023-04-14 15:32:03 +0200

    Modifiche alle pagine di contenuti

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,59 +1,48 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: %i[ show edit update destroy ]
 
-  # GET /contents or /contents.json
+  
   def index
     @contents = Content.all
   end
 
-  # GET /contents/1 or /contents/1.json
   def show
   end
 
-  # GET /contents/new
   def new
     @content = Content.new
   end
 
-  # GET /contents/1/edit
   def edit
   end
 
-  # POST /contents or /contents.json
   def create
     @content = Content.new(content_params)
 
     respond_to do |format|
       if @content.save
-        format.html { redirect_to content_url(@content), notice: "Content was successfully created." }
-        format.json { render :show, status: :created, location: @content }
+        format.html { redirect_to content_url(@content), notice: "L'articolo è stato caricato correttamente!" }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @content.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /contents/1 or /contents/1.json
   def update
     respond_to do |format|
       if @content.update(content_params)
         format.html { redirect_to content_url(@content), notice: "Content was successfully updated." }
-        format.json { render :show, status: :ok, location: @content }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @content.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /contents/1 or /contents/1.json
   def destroy
     @content.destroy
 
     respond_to do |format|
       format.html { redirect_to contents_url, notice: "Content was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
